f91d30a822d66981e981eff4c04b9d5f83c896bf
Dmytro Medynskyi <grandima@gmail.com>
2023-01-04 01:36:20 +0100

    implement 1_1

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,38 @@
+use std::collections::HashSet;
+
+#[derive(Copy, Clone, Default, PartialEq, Eq, Hash)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+#[derive(Clone)]
+struct Polyline {
+    my_set: HashSet<Point>
+}
+impl Polyline {
+    pub fn new(point: Point) -> Self {
+        let mut set = HashSet::new();
+        set.insert(point);
+        Self { my_set: set }
+    }
+    pub fn add(&mut self, point: Point) -> bool {
+        self.my_set.insert(point)
+    }
+}
+#[test]
+fn test_same() {
+    let a = Point::default();
+    let a_same = a;
+    let mut line = Polyline::new(a_same);
+    assert!(!line.add(a))
+}
+#[test]
+fn test_different() {
+    let a = Point::default();
+    let b = Point{x: 2, y :4};
+    let mut line = Polyline::new(a);
+    assert!(line.add(b))
+}
 fn main() {
-    println!("Implement me!");
+    println!("Implement weeme!");
 }
