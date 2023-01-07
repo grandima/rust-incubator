6f2c729d789c5de6b2bfc87a52352024c17bfb6f
Dmytro Medynskyi <dmytro.medynskyi@skelia.com>
2023-01-07 01:55:29 +0100

    add tests

--- 1_concepts/1_2_box_pin/src/main.rs
+++ 1_concepts/1_2_box_pin/src/main.rs
@@ -5,5 +5,11 @@ mod mut_me_vec;
 mod mut_me_string;
 mod mut_me_slice;
 fn main() {
+    use std::pin::Pin;
+    use mut_me_string::*;
+    let mut s = String::from("AAA");
+        let pin = Pin::new(&mut s);
+        pin.mut_me_somehow();
+        println!("{:?}", s.chars().last());
     println!("Implement me!");
 }
