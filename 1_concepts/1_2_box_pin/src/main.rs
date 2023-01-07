c5f69fbdd327792f510eb70ab0c6f47d32a3a501
Dmytro Medynskyi <dmytro.medynskyi@skelia.com>
2023-01-07 12:24:48 +0100

    add slice test

--- 1_concepts/1_2_box_pin/src/main.rs
+++ 1_concepts/1_2_box_pin/src/main.rs
@@ -1,15 +1,15 @@
-mod mut_me_t;
 mod mut_me_box;
 mod mut_me_rc;
-mod mut_me_vec;
-mod mut_me_string;
 mod mut_me_slice;
+mod mut_me_string;
+mod mut_me_t;
+mod mut_me_vec;
 fn main() {
+    use mut_me_slice::*;
     use std::pin::Pin;
-    use mut_me_string::*;
-    let mut s = String::from("AAA");
-        let pin = Pin::new(&mut s);
-        pin.mut_me_somehow();
-        println!("{:?}", s.chars().last());
-    println!("Implement me!");
+    let x = [1 as u8, 2, 3, 4];
+    let mut slice = &x[0..];
+    let pin = Pin::new(&mut slice);
+    pin.mut_me_somehow();
+    println!("{:?}", slice);
 }
