65b3cc88ebaa89a9429761d7f124a849fd108e6e
Dmytro Medynskyi <dmytro.medynskyi@skelia.com>
2023-01-23 17:40:41 +0100

    implement 1_4

--- 1_concepts/1_4_cow/src/main.rs
+++ 1_concepts/1_4_cow/src/main.rs
@@ -1,3 +1,25 @@
+use std::io::prelude::*;
+use std::{
+    borrow::{Cow},
+    env,
+    fs::File,
+};
+fn create_file() {
+    let args: Vec<String> = env::args().collect();
+    let path: Cow<str> = if let Some(i) = args.iter().position(|x| x == "--conf") {
+        args[i + 1].clone().into()
+    } else if let Ok(env_value) = env::var("APP_CONF") {
+        Cow::Owned(env_value)
+    } else {
+        "/etc/app/app.conf".into()
+    };
+    match File::create(path.as_ref()) {
+        Ok(mut file) => {let _ = file.write_all(path.as_ref().as_bytes());},
+        Err(e) => {println!("{:?} {:?}", e, path);},
+    };
+}
+
+
 fn main() {
-    println!("Implement me!");
+    create_file();
 }
