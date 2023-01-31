f8f745018e4e2df33abca90b1c87ba0415a58fcc
Dmytro Medynskyi <dmytro.medynskyi@skelia.com>
2023-01-31 23:45:19 +0100

    implement repos

--- 1_concepts/1_6_dispatch/src/main.rs
+++ 1_concepts/1_6_dispatch/src/main.rs
@@ -1,5 +1,12 @@
+use crate::storage_traits::*;
+use std::collections::HashMap;
 mod storage_traits;
 
 fn main() {
-    println!("Implement me!");
+    let hm = HashMap::new();
+    let storage = StorageImpl(hm);
+    let repo = UserRepositoryDyn::new(Box::new(storage));
+    repo.get(&1);
+    // let repo: UserRepository<u32> = UserRepository{};
+    println!("Imple2ment me!");
 }
--- 1_concepts/1_6_dispatch/src/storage_traits.rs
+++ 1_concepts/1_6_dispatch/src/storage_traits.rs
@@ -4,8 +4,8 @@ pub trait Storage<K, V> {
     fn get(&self, key: &K) -> Option<&V>;
     fn remove(&mut self, key: &K) -> Option<V>;
 }
-pub struct StorageImpl<K: Eq+Hash, V>(HashMap<K, V>);
-impl <K : Eq + Hash, V> StorageImpl<K, V> {
+pub struct StorageImpl<K: Eq+Hash, V>(pub HashMap<K, V>);
+impl <K : Eq + Hash, V> Storage<K, V> for StorageImpl<K, V> {
     fn set(&mut self, key: K, val: V) {
         self.0.insert(key, val);
     }
@@ -22,14 +22,44 @@ pub struct User {
     email: Cow<'static, str>,
     activated: bool,
 }
-pub struct UserRepository<K, V> {
-    storage: dyn Storage<K, V>
+pub struct UserRepositoryDyn {
+    storage: Box<dyn Storage<u64, User>>
 }
-// impl UserRepository {
-//     type Error;
-    
-//     fn get<'a>(&self, key: &u64) -> Option<&'a User>;
-//     fn add(&mut self, user: User) -> Result<(), Self::Error>;
-//     fn update(&mut self, user: User) -> Result<(), Self::Error>;
-//     // and so on
-// }
+impl UserRepositoryDyn {
+    pub fn new(storage: Box<dyn Storage<u64, User>>) -> Self {
+        Self {storage: storage}
+    }
+    pub fn get(&self, key: &u64) -> Option<&User> {
+        self.storage.get(key)
+    }
+    pub fn add(&mut self, user: User) {
+        self.storage.set(user.id, user);
+    }
+    pub fn update(&mut self, user: User) {
+        self.storage.set(user.id, user);
+    }
+    pub fn remove(&mut self, user: User) {
+        self.storage.remove(&user.id);
+    }
+}
+
+pub struct UserRepositoryStatic {
+    storage: StorageImpl<u64, User>
+}
+impl UserRepositoryStatic {
+    pub fn new(storage: StorageImpl<u64, User>) -> Self {
+        Self {storage: storage}
+    }
+    pub fn get(&self, key: &u64) -> Option<&User> {
+        self.storage.get(key)
+    }
+    pub fn add(&mut self, user: User) {
+        self.storage.set(user.id, user);
+    }
+    pub fn update(&mut self, user: User) {
+        self.storage.set(user.id, user);
+    }
+    pub fn remove(&mut self, user: User) {
+        self.storage.remove(&user.id);
+    }
+}
