9af1f5e9307cd423b207d079af11c0aef40059ce
Dmytro Medynskyi <grandima@gmail.com>
2023-02-04 11:48:07 +0100

    update comments

--- 1_concepts/1_7_sized/src/main.rs
+++ 1_concepts/1_7_sized/src/main.rs
@@ -43,17 +43,34 @@ trait CommandHandler<C: Command> {
 }
 
 impl CommandHandler<CreateUser> for User {
-    type Context = dyn Storage<u64, User>;
+    type Context = dyn UserRepository;
     type Result = bool;
     
     fn handle_command(&self, cmd: &CreateUser, ctx: &mut Self::Context) -> Self::Result {
         if cmd.should_deactivate_in_storage {
             let mut user = self.clone();
             user.activated = false;
-            ctx.set(self.id, user)
+            ctx.add(user);
         } else {
-            ctx.set(self.id, self.clone())
+            ctx.add(self.clone());
         }
+        true
+    }
+}
+pub trait UserRepository {
+    fn get(&self, key: &u64) -> Option<&User>;
+    fn add(&mut self, user: User) -> bool;
+}
+pub struct UserRepositoryImpl<S: Storage<u64, User>> {
+    storage: S
+}
+
+impl <S: Storage<u64, User>>UserRepository for UserRepositoryImpl<S> {
+    fn get(&self, key: &u64) -> Option<&User> {
+        self.storage.get(key)
+    }
+    fn add(&mut self, user: User) -> bool {
+        self.storage.set(user.id, user);
         true
     }
 }
@@ -81,13 +98,14 @@ mod tests {
     #[test]
     fn test_mocked() {
         let mut mocked_storage: MockStorageImpl<u64, User> = MockStorageImpl(vec![]);
+        let mut user_repo = UserRepositoryImpl{storage: mocked_storage};
         let c = CreateUser{ should_deactivate_in_storage: false};
         let user = User{id: 1, email: "aa@gmail.com".into(), activated: true};
-        user.handle_command(&c, &mut mocked_storage);
-        assert!(mocked_storage.get(&1).unwrap().activated);
+        user.handle_command(&c, &mut user_repo);
+        assert!(user_repo.get(&1).unwrap().activated);
         let c = CreateUser{ should_deactivate_in_storage: true};
-        user.handle_command(&c, &mut mocked_storage);
-        assert!(!mocked_storage.get(&1).unwrap().activated);
+        user.handle_command(&c, &mut user_repo);
+        assert!(!user_repo.get(&1).unwrap().activated);
     }
 }
 fn main() {
