cadfd7c18e2082d556cbf31d5db04bfd75faa680
Dmytro Medynskyi <dmytro.medynskyi@skelia.com>
2023-02-05 22:49:23 +0100

    implement 1_9

--- 1_concepts/1_9_phantom/src/main.rs
+++ 1_concepts/1_9_phantom/src/main.rs
@@ -1,3 +1,37 @@
+use std::{marker::PhantomData};
+
+use rand::Rng;
+
+struct Fact<T> {
+    _marker: PhantomData<*const T>,
+}
+impl <T> Fact<T> {
+    fn new() -> Fact<T> {
+        Self {
+            _marker: PhantomData,
+        }
+    }
+}
+impl <T> Fact<Vec<T>> {
+    fn fact(&self) -> String {
+        format!("vec fact: {}", rand::thread_rng().gen_range(1..5)).to_string()
+    }
+}
+
+impl Fact<String> {
+    fn fact(&self) -> String {
+        format!("string fact: {}", rand::thread_rng().gen_range(1..5)).to_string()
+    }
+}
+impl Fact<i32> {
+    fn fact(&self) -> String {
+        let mut rng = rand::thread_rng();
+        format!("i32 fact: {}", rng.gen_range(1..4)).to_string()
+    }
+}
+
 fn main() {
-    println!("Implement me!");
+    println!("{}", Fact::<Vec<i32>>::new().fact());
+    println!("{}", Fact::<i32>::new().fact());
+    println!("{}", Fact::<String>::new().fact());
 }
